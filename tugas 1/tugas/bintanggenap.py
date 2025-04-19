f83f736fc66e159627dc3db14622ab7c15a52c93
Septiaagusrianto <rianrian96603@gmail.com>
2025-04-19 14:44:21 +0800

    Add files via upload

---
+++ tugas 1/tugas/bintanggenap.py
@@ -0,0 +1,4 @@
+rows = 5  # Tinggi segitiga
+
+for i in range(1, rows + 1):
+    print(" " * (rows - i) + "*" * (2 * i - 1))
---
+++ tugas 1/tugas/ganjil.py
@@ -0,0 +1,12 @@
+#Inisialisasi jumlah deret
+jumlah = 10
+
+#Inisialisasi bilangan genap awal
+angka = 2
+hitung = 0
+
+#Perulangan while untuk mencetak bilangan genap
+while hitung < jumlah:
+    print(angka, end=", " if hitung < jumlah - 1 else "")
+    angka += 2
+    hitung += 1
